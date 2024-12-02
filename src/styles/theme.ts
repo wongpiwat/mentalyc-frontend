f9e7d4e8ed61cd0fc1e9f51266427072f74b0254
wongpiwat <wongpiwat.san@gmail.com>
2024-12-02 15:52:05 -0500

    implement custom variants on typography

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,15 +1,53 @@
 import { createTheme, ThemeOptions } from "@mui/material";
+import { TypographyOptions } from "@mui/material/styles/createTypography";
+import React from "react";
+
+declare module "@mui/material/Typography" {
+  interface TypographyPropsVariantOverrides {
+    body: true;
+    label: true;
+    small: true;
+  }
+}
+
+interface ExtendedTypographyOptions extends TypographyOptions {
+  body: React.CSSProperties;
+  label: React.CSSProperties;
+  small: React.CSSProperties;
+}
 
 const config: ThemeOptions = {
   components: {
     MuiButton: {
       styleOverrides: {
         root: {
-          textTransform: 'none',
+          textTransform: "none",
         },
       },
     },
   },
+  typography: {
+    h4: {
+      fontFamily: "Inter",
+      fontSize: 20,
+      fontWeight: 700,
+    } as ExtendedTypographyOptions["h4"],
+    body: {
+      fontFamily: "Inter",
+      fontSize: 16,
+      fontWeight: 400,
+    } as ExtendedTypographyOptions["body"],
+    label: {
+      fontFamily: "Inter",
+      fontSize: 14,
+      fontWeight: 400,
+    } as ExtendedTypographyOptions["label"],
+    small: {
+      fontFamily: "Inter",
+      fontSize: 12,
+      fontWeight: 400,
+    } as ExtendedTypographyOptions["small"],
+  } as ThemeOptions["typography"],
   palette: {
     primary: {
       main: "#731054",
