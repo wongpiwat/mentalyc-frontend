3b7df7a17f08fa25192cd75fb6e798c86b4fd86f
wongpiwat <wongpiwat.san@gmail.com>
2024-12-02 23:54:19 -0500

    change client schema and options

--- src/constants/client.ts
+++ src/constants/client.ts
@@ -11,3 +11,22 @@ export const CLIENT_TYPE: { [key: string]: ClientType } = {
   CHILD: { label: "Child", value: 4, color: "success" },
   GROUP: { label: "Group", value: 5, color: "warning" },
 };
+
+
+
+export const DIAGNOSIS_OPTIONS = [
+  { label: "Adjustment disorder with anxiety", tag: "F43.22", value: 1 },
+  { label: "Anxiety", tag: "F42.54", value: 2 },
+  { label: "Disorder", tag: "F21.276", value: 3 },
+];
+
+export const CLIENT_TYPE_OPTIONS = [
+  { label: "Individual", value: "INDIVIDUAL" },
+  { label: "Couple", value: "COUPLE" },
+];
+
+export const PRONOUNS_OPTIONS = [
+  { label: "He/Him", value: "HE_HIM" },
+  { label: "She/Her", value: "SHE_HER" },
+  { label: "They/Them", value: "THEY_THEM" },
+];
--- src/schemas/client.schema.ts
+++ src/schemas/client.schema.ts
@@ -1,28 +1,29 @@
 import { z as zod } from "zod";
 import { nanoid } from "nanoid";
+import { CLIENT_TYPE_OPTIONS, DIAGNOSIS_OPTIONS, PRONOUNS_OPTIONS } from "@/constants/client";
 
 export const clientSchema = zod.object({
   id: zod.string(),
-  clientType: zod.string().min(1, { message: "clientType is required" }),
-  clientName: zod.string().min(1, { message: "clientName is required" }),
-  clientName2: zod.string().min(1, { message: "clientName2 is required" }),
-  pronouns: zod.string().min(1, { message: "pronouns is required" }),
-  yearOfBirth: zod.string().min(1, { message: "yearOfBirth is required" }),
-  diagnosis: zod.string().min(1, { message: "diagnosis is required" }),
+  clientType: zod.string().min(1, { message: "Client type is required" }),
+  clientName: zod.string().min(1, { message: "Client name is required" }),
+  clientName2: zod.string().optional(),
+  pronouns: zod.string().min(1, { message: "Pronouns is required" }),
+  yearOfBirth: zod.string().optional(),
+  diagnosis: zod.number().min(0, { message: "Diagnosis is required" }),
   highRiskClient: zod.boolean(),
-  extraNotes: zod.string().min(0, { message: "extraNotes is required" }),
+  extraNotes: zod.string().optional(),
 });
 
 export type Values = zod.infer<typeof clientSchema>;
 
 export const defaultValues = {
   id: nanoid().toString().slice(0, 7), // Generate random id with 7 characters
-  clientType: "",
+  clientType: CLIENT_TYPE_OPTIONS[0].value,
   clientName: "",
   clientName2: "",
-  pronouns: "",
+  pronouns: PRONOUNS_OPTIONS[0].value,
   yearOfBirth: "",
-  diagnosis: "",
+  diagnosis: DIAGNOSIS_OPTIONS[0].value,
   highRiskClient: false,
   extraNotes: "",
 } satisfies Values;
